28fb005580a29cc8be0fdd987f2c98fb025f0917
anshkush92college <imt_2020013@iiitm.ac.in>
2022-12-28 22:55:04 +0530

    Created a linear gradient on scrolling down, also now giving inside padding instead giving whole layout the padding, otherwise, we won't be able to give the gradient to the whole width

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -3,10 +3,10 @@ import { Footer } from './Footer';
 
 export const Layout = ({ children }: any) => {
   return (
-    // For giving padding to the whole Layout
+    // For giving padding to the whole Layout ----> px-8 py-4
     <div>
       <Navbar />
-      <main className="px-8 py-2">{children}</main>
+      <main>{children}</main>
       <Footer />
     </div>
   );
