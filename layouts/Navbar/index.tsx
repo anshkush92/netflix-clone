6b3e10646c49e3137782df5bc11a122323979ada
anshkush92college <imt_2020013@iiitm.ac.in>
2022-12-28 22:40:34 +0530

    Added the functionality to change the navbar color / bg color when it is scrolled down

--- layouts/Navbar/index.tsx
+++ layouts/Navbar/index.tsx
@@ -1,5 +1,5 @@
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
-import React from 'react';
 import { AiOutlineSearch, AiOutlineBell } from 'react-icons/ai';
 
 import { navbarLinks } from '../../utils/constants';
@@ -7,8 +7,29 @@ import { navbarLinks } from '../../utils/constants';
 /* Rules to disable the warning when using <img /> instead of <Image /> */
 /* eslint-disable @next/next/no-img-element */
 export const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  // This useEffet will be used to change the navbar color when the user scrolls down
+  useEffect(() => {
+    console.log('useEffect called');
+    const handleScroll = () => {
+      // Means the user has scrolled down (even by a single pixel)
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    // Adds the event listener to the window object to listen for scroll events
+    window.addEventListener('scroll', handleScroll);
+
+    // Cancels the event listener when the component is unmounted, so that it doesn't cause any memory leaks
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <header className="navbar">
+    <nav className={`navbar ${isScrolled && 'text-red-600'}`}>
       {/* Left Section of the Navbar */}
       <div className="flex items-center gap-x-5 md:gap-x-10">
         <img
@@ -41,6 +62,6 @@ export const Navbar = () => {
           />
         </Link>
       </div>
-    </header>
+    </nav>
   );
 };
