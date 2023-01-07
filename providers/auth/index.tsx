4dba8d3ef639ad04494519c293d2059371184a26
anshkush92college <imt_2020013@iiitm.ac.in>
2023-01-07 23:23:00 +0530

    Fixed the bug of infinite re-renders in the useEffect() because of the router dependecy, see the stackoverflow link above the useEffect()

--- providers/auth/index.tsx
+++ providers/auth/index.tsx
@@ -1,5 +1,5 @@
-import { useRouter } from 'next/router';
-import { createContext, useContext, useEffect, useMemo, useState } from 'react';
+import Router, { useRouter } from 'next/router';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 
 import {
   createUserWithEmailAndPassword,
@@ -21,62 +21,81 @@ const AuthProvider = ({ children }: AuthProviderProps) => {
   const [isLoading, setIsLoading] = useState(false);
   // Getting the user type from the firebase
   const [user, setUser] = useState<User | null>(null);
+  console.log('🚀 ~ file: index.tsx:24 ~ AuthProvider ~ user', user);
 
   const router = useRouter();
+
+  const memoizedValue = useMemo(
+    () => ({
+      user,
+      loading: isLoading,
+    }),
+    [user, isLoading]
+  );
+
   /**
    * @param email - Email of the user
    * @param password - Password of the user
    * @function register - Function to register a user
    */
-  const register = async (email: string, password: string) => {
-    setIsLoading(true);
-    try {
-      // Creating a user with email and password
-      const result = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      console.log('🚀 ~ file: index.tsx:40 ~ register ~ result', result);
-      setUser(result.user);
-      // Redirecting to the home page, after successful sign up
-      router.push('/login');
-      setIsLoading(false);
-    } catch (error) {
-      console.log('🚀 ~ file: index.tsx:39 ~ register ~ error', error);
-    } finally {
-      // This is always going to run no matter what happens
-      setIsLoading(false);
-    }
-  };
+  const register = useCallback(
+    async (email: string, password: string) => {
+      console.log('🚀 ~ Inside register function');
+      setIsLoading(true);
+      try {
+        // Creating a user with email and password
+        const result = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+        console.log('🚀 ~ file: index.tsx:40 ~ register ~ result', result);
+        setUser(result.user);
+        // Redirecting to the home page, after successful sign up
+        router.push('/login');
+        setIsLoading(false);
+      } catch (error) {
+        console.log('🚀 ~ file: index.tsx:39 ~ register ~ error', error);
+      } finally {
+        // This is always going to run no matter what happens
+        setIsLoading(false);
+      }
+    },
+    [router]
+  );
 
   /**
    * @param email - Email of the user
    * @param password - Password of the user
    * @function login - Function to login a user
    */
-  const login = async (email: string, password: string) => {
-    setIsLoading(true);
-    try {
-      // Logging in a user with email and password
-      const result = await signInWithEmailAndPassword(auth, email, password);
-      console.log('🚀 ~ file: index.tsx:63 ~ login ~ result', result);
-      setUser(result.user);
-      // Redirecting to the home page, after successful sign up
-      router.push('/');
-      setIsLoading(false);
-    } catch (error) {
-      console.log('🚀 ~ file: index.tsx:56 ~ login ~ error', error);
-    } finally {
-      // This is always going to run no matter what happens
-      setIsLoading(false);
-    }
-  };
+  const login = useCallback(
+    async (email: string, password: string) => {
+      console.log('🚀 ~ file: Inside login function');
+      setIsLoading(true);
+      try {
+        // Logging in a user with email and password
+        const result = await signInWithEmailAndPassword(auth, email, password);
+        console.log('🚀 ~ file: index.tsx:63 ~ login ~ result', result);
+        setUser(result.user);
+        // Redirecting to the home page, after successful sign up
+        router.push('/');
+        setIsLoading(false);
+      } catch (error) {
+        console.log('🚀 ~ file: index.tsx:56 ~ login ~ error', error);
+      } finally {
+        // This is always going to run no matter what happens
+        setIsLoading(false);
+      }
+    },
+    [router]
+  );
 
   /**
    * @function logout - Function to logout a user
    */
-  const logout = async () => {
+  const logout = useCallback(async () => {
+    console.log('🚀 ~ file: Inside logout function');
     setIsLoading(true);
     try {
       // Logging out the user
@@ -90,37 +109,32 @@ const AuthProvider = ({ children }: AuthProviderProps) => {
     } finally {
       setIsLoading(false);
     }
-  };
-
-  // Listening to the auth state changes and persisting the user, as user goes to null if refreshed, accepts the "auth" instance and gives back the "user"
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // Means the user is already logged in
-        setUser(user);
-        setIsLoading(false);
-      } else {
-        // Means the user is not logged in, so we move to the login page
-        setUser(null);
-        setIsLoading(true);
-        router.push('/login');
-      }
-    });
   }, [router]);
 
-  return (
-    <AuthContext.Provider
-      value={{
-        user,
-        register,
-        login,
-        logout,
-        loading: isLoading,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
+  /**
+   * @tutorial https://stackoverflow.com/questions/69203538/useeffect-dependencies-when-using-nextjs-router - How to avoid the infinite re-renders when using the useRouter in useEffect()
+   * @description -  Listening to the auth state changes and persisting the user, as user goes to null if refreshed, accepts the "auth" instance and gives back the "user"
+   */
+  useEffect(
+    () =>
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          // Logged in...
+          setUser(user);
+          setIsLoading(false);
+        } else {
+          // Not logged in...
+          setUser(null);
+          setIsLoading(true);
+          Router.push('/login');
+        }
+      }),
+    []
   );
+
+  const value = { ...memoizedValue, register, login, logout };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
 export default AuthProvider;
